7478691a418792ab68f63f6ca01c9e24940c3062
Alibek <alibek.mem@mail.ru>
2020-11-11 11:19:37 +0600

    small changes with cls

--- SpeechRecognition/SpeechRecognition/Functions.py
+++ SpeechRecognition/SpeechRecognition/Functions.py
@@ -1,61 +1,61 @@
-import os, winshell
-import speech_recognition as sr
-import Functions
-from gtts import gTTS
+#import os, winshell
+#import speech_recognition as sr
+#import Functions
+#from gtts import gTTS
 
-# Воспроизведение речи
-import pygame
-from pygame import mixer
-try:
-    mixer.init()
-except pygame.error:
-    print("Аудио девайс не опознан");
+## Воспроизведение речи
+#import pygame
+#from pygame import mixer
+#try:
+#    mixer.init()
+#except pygame.error:
+#    print("Аудио девайс не опознан");
 
-import os
-import sys
-import time
-import datetime
-import logging
-import webbrowser
-import subprocess
-import sqlite3
+#import os
+#import sys
+#import time
+#import datetime
+#import logging
+#import webbrowser
+#import subprocess
+#import sqlite3
 
-class Functions():
-    def InsertAdress():
-        print("Скаижите адрес ( пример : 'Сатпаева 25 это дом' )")
-        with self._microphone as source:
-            audio = self._recognizer.listen(source)
+#class Functions():
+#    def InsertAdress():
+#        print("Скаижите адрес ( пример : 'Сатпаева 25 это дом' )")
+#        with self._microphone as source:
+#            audio = self._recognizer.listen(source)
 
-            adres = statement[0:statement.find("это")]
-            name = statement[statement.find("это") : statement.length]
+#            adres = statement[0:statement.find("это")]
+#            name = statement[statement.find("это") : statement.length]
 
-            con.create_aggregate("name", 1, name)
-            con.create_aggregate("adres", 2, adres)
+#            con.create_aggregate("name", 1, name)
+#            con.create_aggregate("adres", 2, adres)
 
-            c.execute("insert into adreses(?,?) values(1,2)")
+#            c.execute("insert into adreses(?,?) values(1,2)")
 
-    def InsertEvent():
-        print("Скажите дату и имя мероприятия (пример: 'день рождение Димы дата 20 3 2019')")
-        with self._microphone as source:
-            audio = self._recognizer.listen(source)
+#    def InsertEvent():
+#        print("Скажите дату и имя мероприятия (пример: 'день рождение Димы дата 20 3 2019')")
+#        with self._microphone as source:
+#            audio = self._recognizer.listen(source)
 
-            event = statement[0:statement.find("дата")]
-            name = statement[statement.find("дата") : statement.length]
+#            event = statement[0:statement.find("дата")]
+#            name = statement[statement.find("дата") : statement.length]
 
-            con.create_aggregate("name", 1, name)
-            con.create_aggregate("event", 2, event)
+#            con.create_aggregate("name", 1, name)
+#            con.create_aggregate("event", 2, event)
 
-            c.execute("insert into adreses(?,?) values(1,2)")
+#            c.execute("insert into adreses(?,?) values(1,2)")
 
-    def WriteFile():
-        print("Скажите чтох записать в блокнот: ")
-        with self._microphone as source:
-            audio = self._recognizer.listen(source)
+#    def WriteFile():
+#        print("Скажите чтох записать в блокнот: ")
+#        with self._microphone as source:
+#            audio = self._recognizer.listen(source)
              
-            desktop = winshell.desktop()
-            path = os.path.join(desktop, "Твоя запись.txt")            
+#            desktop = winshell.desktop()
+#            path = os.path.join(desktop, "Твоя запись.txt")            
             
-            shortcut = file(path, 'w') # это из версии 2.х
-            shortcut.write(statment)
-            shortcut.close()
+#            shortcut = file(path, 'w') # это из версии 2.х
+#            shortcut.write(statment)
+#            shortcut.close()
 
--- SpeechRecognition/SpeechRecognition/Speech_AI.py
+++ SpeechRecognition/SpeechRecognition/Speech_AI.py
@@ -1,6 +1,4 @@
 import speech_recognition as sr
-import Functions
-from gtts import gTTS
 import random
 import time
 # Воспроизведение речи
@@ -19,6 +17,7 @@ import logging
 import webbrowser
 import subprocess
 import sqlite3
+cls = lambda: os.system('cls')
 class Speech_AI:
 
     def __init__(self):
@@ -28,10 +27,6 @@ class Speech_AI:
         except OSError:
             print("Микрофон не опознан");
         
-        now_time = datetime.datetime.now()
-        self._mp3_name = now_time.strftime("%d%m%Y%I%M%S")+".mp3"
-        self._mp3_nameold='111'
-
     def start(self):
         try:
             print("Минутку тишины, пожалуйста...")
@@ -49,15 +44,16 @@ class Speech_AI:
                       return True;
                   print("Вы сказали: {}".format(statement))
                 except sr.UnknownValueError:
+                   cls()
                    print("Упс! Кажется, я тебя не поняла, повтори еще раз")
                 except sr.RequestError as e:
                   print("Не могу получить данные от сервиса Google Speech Recognition; {0}".format(e))
                 except TimeoutError:
                   print("Время ожидания ответа из сервера истекло")
                   return 0;
-        except KeyboardInterrupt:
-            self._clean_up()
+        except:
             print("Пока!")
+            return 0;
 
     def work(self):
         print("Минутку тишины, пожалуйста...")
@@ -79,56 +75,61 @@ class Speech_AI:
 
                     if((statement.find("калькулятор")!=-1) or (statement.find("calculator")!=-1)):
                         self.osrun('calc')
-                               
+                        cls()
+
                     if((statement.find("блокнот")!=-1) or (statement.find("notepad")!=-1)):
                         self.osrun('notepad')
+                        cls()
                              
                     if((statement.find("paint")!=-1) or (statement.find("паинт")!=-1)):
                         self.osrun('mspaint')
+                        cls()
 
                     if((statement.find("диспетчер задач")!=-1)):
                         self.osrun('taskmgr')
+                        cls()
                         
                     if((statement.find("командная строка")!=-1)):
                         self.osrun('start cmd')
+                        cls()
                     
                     if((statement.find("проигрыватель виндоус")!=-1) or (statement.find("проигрыватель windows")!=-1)):
                         self.osrun('start wmplayer')
+                        cls()
 
                     if((statement.find("browser")!=-1) or (statement.find("браузер")!=-1)):
+                        cls()
                         self.openurl('http://google.ru', 'Открываю браузер')
  
                     # Команды для открытия URL в браузере
                     
                     if(((statement.find("youtube")!=-1) or (statement.find("youtub")!=-1) or (statement.find("ютуб")!=-1) or (statement.find("you tube")!=-1)) and (statement.find("смотреть")==-1)):                        
+                        cls()
                         self.openurl('http://youtube.com', 'Открываю ютуб')
  
                     if(((statement.find("новости")!=-1) or (statement.find("новость")!=-1) or (statement.find("на усть")!=-1))):
+                        cls()
                         self.openurl('https://www.nur.kz', 'Открываю новости')
                          
                     if((statement.find("mail")!=-1) or (statement.find("майл")!=-1)):
+                        cls()
                         self.openurl('https://e.mail.ru/messages/inbox/', 'Открываю почту')
                         
                     if((statement.find("вконтакте")!=-1) or (statement.find("в контакте")!=-1)):
+                        cls()
                         self.openurl('http://vk.com', 'Открываю Вконтакте')
 
-                    if((statement.find("майстат")!=-1) or (statement.find("май стат")!=-1) or (statement.find("mystat")!=-1) or (statement.find("my stat")!=-1)):
-                        self.openurl('https://mystat.itstep.org/en/main', 'Открываю Mystat')
-
                     if((statement.find("телеграмм")!=-1) or (statement.find("телеграм")!=-1) or (statement.find("telegram")!=-1)):
+                        cls()
                         self.openurl('https://web.telegram.org', 'Открываю Telegram')
 
-                    if((statement.find("голосование")!=-1)):
-                        self.openurl('https://www.youtube.com/watch?v=3eToZUkGluk', 'Открываю Рикардо Милоса')
-
-                    if((statement.find("покажи бога")!=-1)):
-                        self.openurl('https://pbs.twimg.com/profile_images/679621054361239552/X8Xu2CHm_400x400.jpg', 'Показывают бога')
-
                     if((statement.find("случайное аниме")!=-1) or (statement.find("рандом аниме")!=-1)):
+                        cls()
                         self.openurl('https://yummyanime.com/random', 'Вот случайное аниме')
 
                     if((statement.find("включи музыку")!=-1) or (statement.find("включи музыки")!=-1)):
-                        self.openurl('https://www.youtube.com/watch?v=hHW1oY26kxQ', 'Тихий хип-хоп')
+                        cls()
+                        self.openurl('https://www.youtube.com/watch?v=5qap5aO4i9A&ab_channel=ChilledCow', 'Тихий хип-хоп')
 
                     # Команды для поиска в сети интернет
                   
@@ -137,6 +138,7 @@ class Speech_AI:
                         statement=statement.replace('найти', '')
                         statement=statement.replace('поиск', '')
                         statement=statement.strip()
+                        cls()
                         self.openurl('https://yandex.ru/yandsearch?text=' + statement, "Я нашла следующие результаты")
                         
                     if((statement.find("смотреть")!=-1) and ((statement.find("фильм")!=-1) or (statement.find("film")!=-1))):
@@ -146,6 +148,7 @@ class Speech_AI:
                         statement=statement.replace('фильм', '')
                         statement=statement.replace('film', '')
                         statement=statement.strip()
+                        cls()
                         self.openurl('https://yandex.ru/yandsearch?text=Смотреть+онлайн+фильм+' + statement, "Выберите сайт где смотреть фильм")
 
                     if(((statement.find("youtube")!=-1) or (statement.find("ютуб")!=-1) or (statement.find("you tube")!=-1)) and (statement.find("смотреть")!=-1)):
@@ -161,44 +164,12 @@ class Speech_AI:
                         statement=statement.replace('посмотреть', '')
                         statement=statement.replace('смотреть', '')
                         statement=statement.strip()
+                        cls()
                         self.openurl('http://www.youtube.com/results?search_query=' + statement, 'Ищу в ютуб')
 
 
-                    #if((statement.find("слушать")!=-1) and (statement.find("песни")!=-1)):
-                    #    statement=statement.replace('песню', '')
-                    #    statement=statement.replace('песни', '')
-                    #    statement=statement.replace('песня', '')
-                    #    statement=statement.replace('песней', '')
-                    #    statement=statement.replace('послушать', '')
-                    #    statement=statement.replace('слушать', '')
-                    #    statement=statement.replace('хочу', '')
-                    #    statement=statement.strip()
-                    #    self.openurl('https://vk.com/audios356018751?q=' + statement, "Нажмите плэй")
-
-
-                    #    # бд
-                    #if((statement.find("запомни")) != -1 or (statement.find("запиши") != 1)):
-                    #    conn = sqlite3.connect("Data.db")
-                    #    c = conn.cursor()
-                    #    if(statement.find("адрес") != 1):
-                    #       Functions.InsertAdress()
-                            
-                    #    if((statement.find("событи") != 1) or (statment.find("день рождени") != 1) or (statment.find("мероприяти") != 1)):
-                    #        Functions.InsertEvent()
-
-
-                    #    # файловая система
-
-                            
-                    #if((statement.find("пиши") != -1) and (statement.find("блокнот")!=-1)):
-                    #    Functions.WriteFile()
-                            
-                            
-                            # Поддержание диалога
-                    
                     if((statement.find("до свидания")!=-1) or (statement.find("досвидания")!=-1) or (statement.find("пока")!=-1) or (statement.find("увидимся")!=-1) or (statement.find("прощай")!=-1)):
-                        answer = "Пока!"
-                        self.say(str(answer))
+                        print("Всего хорошего, мистер Старк!")
                         while pygame.mixer.music.get_busy():
                             time.sleep(0.1)
                         sys.exit()
@@ -208,7 +179,6 @@ class Speech_AI:
                         hour = str(time_list[3]);
                         minute = str(time_list[4]);
                         print("Сейчас " + hour + ":" + minute);
-                        self.say("Сейчас " + hour + ":" + minute);
 
                     if((statement.find("дата")!=-1) or (statement.find("какое сегодня число")!=-1) or (statement.find("сегоднящнее число")!=-1) or (statement.find("сегоднящняя дата")!=-1)):   
                         time_list = time.localtime()
@@ -216,99 +186,68 @@ class Speech_AI:
                         month = str(time_list[1]);
                         day = str(time_list[2]);
                         print("Сейчас " + year + " год, " + month + " месяц и "+ day + " день");
-                        self.say("Сейчас " + year + " год, " + month + " месяц и "+ day + " день");
                 
                     if((statement.find("привет")!=-1) or (statement.find("здравствуй")!=-1) or (statement.find("приветствую")!=-1) or (statement.find("здорово")!=-1)):
                         time_list = time.localtime()
                         hours = time_list[3]
                         if(6 <= int(hours) <= 12):
-                            self.say("Доброго утра!")
+                            print("Доброго утра!")
                         elif(13 <= int(hours) <= 18):
-                            self.say("Добрый день!")
+                            print("Добрый день!")
                         elif(19 <= int(hours) <= 23):
-                            self.say("Добрый вечер!")
+                            print("Добрый вечер!")
                         else:
-                            self.say("Доброй ночи")
+                            print("Доброй ночи")
                     
                     if((statement.find("как дела")!=-1) or (statement.find("как твои дела")!=-1) or (statement.find("как поживаешь")!=-1) or (statement.find("как идут дела")!=-1)):
                         randAnswer = random.randint(0,2)
                         if(randAnswer==0):
-                            self.say('Я всего лишь программа которая выполняет приказы на которые я способна, так что перестань спрашивать меня как идут у меня дела')
+                            print('Я всего лишь программа которая выполняет приказы на которые я способна, так что перестань спрашивать меня как идут у меня дела')
                         elif(randAnswer==1):
-                            self.say("Хорошо, у вас как?")
+                            print("Хорошо, у вас как?")
                         elif(randAnswer==2):
-                            self.say("Наилучшим образом.")
+                            print("Наилучшим образом.")
                     else:
                         randAnswer = random.randint(0,10)
                         if(randAnswer==0):
-                            self.say("Отлично")
+                            print("Отлично")
                         if(randAnswer==1):
-                            self.say("Я рада")
+                            print("Я рада")
                         if(randAnswer==2):
-                            self.say("Хорошо")
+                            print("Хорошо")
                         if(randAnswer==3):
-                            self.say("А вы знали, что пэйпал это плохая вещь?")
+                            print("Я не знаю")
                         if(randAnswer==4):
-                            self.say("Я не знаю")
+                            print("Как же так вышло?")
                         if(randAnswer==5):
-                            self.say("Как же так вышло?")
+                            print("Ага")
                         if(randAnswer==6):
-                            self.say("Ага")
+                            print("Возможно")
                         if(randAnswer==7):
-                            self.say("Возможно")
+                            print("Я не могу понять вас, я всего лишь робот")
                         if(randAnswer==8):
-                            self.say("Я не могу понять вас, я всего лишь робот")
-                        if(randAnswer==9):
-                            self.say("Гло́кая ку́здра ште́ко будлану́ла бо́кра и курдя́чит бокрёнка")
-                        if(randAnswer==10):
-                            self.say("А?")
+                            print("А?")
                     print("Вы сказали: {}".format(statement))
                     
                 except sr.UnknownValueError:
+                    cls()
                     print("Упс! Кажется, я тебя не поняла, повтори еще раз")
                 except sr.RequestError as e:
                     print("Не могу получить данные от сервиса Google Speech Recognition; {0}".format(e))
                 except TimeoutError:
                     print("Не могу получить данные от сервиса Google Speech Recognition")
                     return 0;
-        except KeyboardInterrupt:
-            self._clean_up()
+        except:
             print("Пока!")
-        
+            return 0;
+            
     def osrun(self, cmd):
         PIPE = subprocess.PIPE
         p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
 
     def openurl(self, url, ans):
         webbrowser.open(url)
-        #self.say(str(ans))
+        print(ans)
         while pygame.mixer.music.get_busy():
             time.sleep(0.1)
-
-    def say(self, phrase):
-        try:
-            tts = gTTS(text=phrase, lang="ru")
-            tts.save(self._mp3_name)
-        except RuntimeError:
-            print("");
-
-        # Play answer
-        try:
-            mixer.music.load(self._mp3_name)
-            mixer.music.play()
-            if(os.path.exists(self._mp3_nameold)):
-                os.remove(self._mp3_nameold)
-        except pygame.error:
-            print("");
-       
-        now_time = datetime.datetime.now()
-        self._mp3_nameold=self._mp3_name
-        self._mp3_name = now_time.strftime("%d%m%Y%I%M%S")+".mp3"
-        
-        
-    
-
-    def _clean_up(self):
-        def clean_up():
-            os.remove(self._mp3_name)
 
